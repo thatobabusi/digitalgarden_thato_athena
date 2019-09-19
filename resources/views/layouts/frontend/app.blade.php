5df6f9d67ff29a1c91f059134f59c3e46c868591
Thato Babusi <thatobabusi@yahoo.com>
2019-09-19 10:15:21 +0200

    refactored layout

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -29,7 +29,6 @@
     <link href="{{ asset('html_theme/hedone/assets/css/hedone.css') }}" rel="stylesheet" type="text/css"/>
     <link href="{{ asset('html_theme/hedone/assets/css/colors.css') }}" rel="stylesheet" type="text/css"/>
 
-
     <!--[if lte IE 9]>
     <link href="{{ asset('html_theme/hedone/assets/css/animations-ie-fix.css') }}" rel="stylesheet" type="text/css"/>
     <![endif]-->
@@ -55,34 +54,7 @@
 @yield('content')
 
 <!-- FOOTER -->
-<footer class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="footer-logo">
-                <img src="{{ asset('html_theme/hedone/assets/img/logo-white.png') }}" alt="footer logo"/>
-            </div>
-            <div class="footer-social-block">
-                <a href="#" class="w-inline-block social-wrap">
-                    <i class="fa fa-twitter"></i>
-                </a>
-                <a href="#" class="w-inline-block social-wrap">
-                    <i class="fa fa-linkedin"></i>
-                </a>
-                <a href="#" class="w-inline-block social-wrap">
-                    <i class="fa fa-facebook"></i>
-                </a>
-                <a href="#" class="w-inline-block social-wrap">
-                    <i class="fa fa-youtube"></i>
-                </a>
-            </div>
-            <div class="footer-text">
-                © We are <span class="footer-text-span">Hedone</span>. All rights reserved. 2017<br>Made by <span
-                    class="footer-text-span">IG Design</span> in Kraljevo, Serbia
-            </div>
-        </div>
-    </div>
-</footer>
-
+@include('layouts.frontend.footer')
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="{{ asset('html_theme/hedone/assets/js/jquery.js') }}"></script>
---
+++ resources/views/layouts/frontend/footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-logo">
+                <img src="{{ asset('html_theme/hedone/assets/img/logo-white.png') }}" alt="footer logo"/>
+            </div>
+            <div class="footer-social-block">
+                <a href="#" class="w-inline-block social-wrap">
+                    <i class="fa fa-twitter"></i>
+                </a>
+                <a href="#" class="w-inline-block social-wrap">
+                    <i class="fa fa-linkedin"></i>
+                </a>
+                <a href="#" class="w-inline-block social-wrap">
+                    <i class="fa fa-facebook"></i>
+                </a>
+                <a href="#" class="w-inline-block social-wrap">
+                    <i class="fa fa-youtube"></i>
+                </a>
+            </div>
+            <div class="footer-text">
+                © We are <span class="footer-text-span">Hedone</span>. All rights reserved. 2017<br>Made by <span
+                    class="footer-text-span">IG Design</span> in Kraljevo, Serbia
+            </div>
+        </div>
+    </div>
+</footer>
